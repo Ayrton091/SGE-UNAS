651b3f67b8e0e18bfe899dc15466e51cfd051e5d
Ayrton091 <riostellojart01@gmail.com>
2024-03-18 17:56:38 -0500

    Añade al alumno a egresados cuando completa los creditos de la E_P

--- Contenedor/App1/models.py
+++ Contenedor/App1/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Escuela_profesional(models.Model):
     nombre = models.CharField(max_length=100)
     cantidad_creditos = models.PositiveSmallIntegerField(validators=[MaxValueValidator(200)])
 
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre} - {self.cantidad_creditos}"
+
 
 
 class Alumno(models.Model):
@@ -22,8 +24,7 @@ class Alumno(models.Model):
     numero_telefono = models.CharField(max_length=9)
 
     def __str__(self):
-        return self.nombre
-
+        return f"{self.nombre} - {self.c_creditos} - {self.Escuela_profesional}"
 
 class Egresado(models.Model):
     codigo_alumno = models.OneToOneField(Alumno, on_delete=models.CASCADE)
@@ -32,6 +33,24 @@ class Egresado(models.Model):
 
     def __str__(self):
         return self.codigo_alumno.nombre
+
+
+
+
+@receiver(post_save, sender=Alumno)
+def verificar_egreso(sender, instance, created, **kwargs):
+    if not created:  # Evitar ejecutar la lógica en la creación de un nuevo alumno
+        escuela_profesional = instance.Escuela_profesional
+        cantidad_creditos = escuela_profesional.cantidad_creditos
+        creditos_alumno = instance.c_creditos
+        if creditos_alumno >= cantidad_creditos:
+            # Crear un registro en la tabla de egresados
+            Egresado.objects.create(
+                codigo_alumno=instance,
+                fecha_egreso=instance.fecha_nacimiento,  # Ajusta esta fecha según sea necesario
+                titulo="Título de egreso"  # Ajusta el título según sea necesario
+            )
+
 
 
 class Trayectoria(models.Model):
